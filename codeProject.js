3d63a30e8b8c3055b55974964b08897367980f82
@nbharuchi <97713275+nbharuchi@users.noreply.github.com>
2024-08-04 11:30:40 -0400

    finish project

---
+++ codeProject.js
@@ -0,0 +1,46 @@
+const container = document.querySelector('.container');
+container.style.display = "flex";
+container.style.flexWrap = "wrap";
+
+function randomNumber(num) {
+    let randomNumber = Math.floor(Math.random() * num);
+    return randomNumber;
+}
+function createGrid(numOfSquares) {
+    for (let i = 0; i < numOfSquares * numOfSquares; i++) {
+        const div = document.createElement("div");
+        div.classList.add("squares");
+        container.appendChild(div);
+        div.style.width = `calc(100% / ${numOfSquares})`;
+        div.style.paddingBottom = `calc(100% / ${numOfSquares})`;
+        div.style.boxSizing = "border-box";
+        //for (let j = 0.0; j < 1; j + 0.1) {
+        //let darkSquare = 0.1;
+        //let opacity = 0;
+        div.addEventListener("mouseover", () => {
+            const randomR = randomNumber(255);
+            const randomG = randomNumber(255);
+            const randomB = randomNumber(255);
+            div.style.backgroundColor = `rgb(${randomR} ${randomG} ${randomB} )`;
+            //div.style.opacity = `${opacity} + ${darkSquare}`;
+        })
+        //}
+
+    }
+}
+
+createGrid(16);
+
+const button = document.querySelector("button");
+button.textContent = "Generate new grid";
+button.addEventListener("click", () => {
+    numOfSquares = prompt("enter a new grid size");
+    if (numOfSquares > 100) {
+        numOfSquares = prompt("enter a new grid size under 100");
+    }
+    container.innerHTML = "";
+    createGrid(numOfSquares);
+
+})
+
+
